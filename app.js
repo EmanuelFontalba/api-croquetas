fe3c63cd5d301de20eff6740c3a9f7ec7fd22b23
EmanuelFontalba <emanuelfontalba@gmail.com>
2020-01-26 13:56:05 +0100

    modify error handler

--- app.js
+++ app.js
@@ -8,11 +8,6 @@ const api = require('./routes')
 
 const app = express();
 
-
-// // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'pug');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -20,6 +15,14 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(api);
+
+app.listen(9000, () => console.log('Listening on 9000'))
+
+
+// // view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'pug');
+
 
 // // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
@@ -33,10 +36,8 @@ app.use(api);
 //   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
 //   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
+//   res.status(err.status || 500).send({status: err.status || 500, message: err.message});
 // });
 
-app.listen(9000, () => console.log('Listening on 9000'))
 
 module.exports = app;
